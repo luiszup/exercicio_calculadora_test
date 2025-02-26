0cc6e313908f248fe1d6c6bdfa089ee14bdb4bb5
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-02-26 16:20:39 -0300

    feat: add modulo method in calculadora class

--- src/main/java/br/com/zup/Calculadora.java
+++ src/main/java/br/com/zup/Calculadora.java
@@ -16,4 +16,8 @@ public class Calculadora {
     public Double multiplicar(Double n1, Double n2) {
         return n1 * n2;
     }
+
+    public Double modulo(Double n1, Double n2) {
+        return n1 % n2;
+    }
 }

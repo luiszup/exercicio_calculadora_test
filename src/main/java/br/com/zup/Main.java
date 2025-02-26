a5487d8795941ab9eb99e02b529031e757715ae1
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-02-26 16:17:24 -0300

    feat: add original main class from exercicio_calculadora

--- src/main/java/br/com/zup/Main.java
+++ src/main/java/br/com/zup/Main.java
@@ -1,7 +1,30 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número: ");
+        double numero1 = input.nextInt();
+        System.out.println("Digite o segundo número: ");
+        double numero2 = input.nextDouble();
+        System.out.println("Digite qual operação realizar (+, -, /, *, %)");
+        char operador = input.next().charAt(0);
+
+        if (operador == '+') {
+            System.out.println(numero1 + " + " + numero2 + " = " + (numero1 + numero2));
+        } else if (operador == '-') {
+            System.out.println(numero1 + " - " + numero2 + " = " + (numero1 - numero2));
+        } else if (operador == '/') {
+            System.out.println(numero1 + " / " + numero2 + " = " + (numero1 / numero2));
+        } else if (operador == '*') {
+            System.out.println(numero1 + " * " + numero2 + " = " + (numero1 * numero2));
+        } else if (operador == '%') {
+            System.out.println(numero1 + " % " + numero2 + " = " + (numero1 % numero2));
+        } else {
+            System.out.println("Operador inválido! Escreva uma das opções apresentadas.");
+        }
     }
 }

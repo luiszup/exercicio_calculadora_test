ddcf78b61debcfe9b049581b3813904ec18dc8b1
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-02-26 16:19:07 -0300

    feat: add subtrair method in calculadora class

--- src/main/java/br/com/zup/Calculadora.java
+++ src/main/java/br/com/zup/Calculadora.java
@@ -4,4 +4,8 @@ public class Calculadora {
     public Double somar(Double n1, Double n2) {
         return n1 + n2;
     }
+
+    public Double subtrair(Double n1, Double n2) {
+        return n1 - n2;
+    }
 }

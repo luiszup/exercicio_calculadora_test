58c1b5088d2ed9f8ab1a6e76ba94c5d4f1dafc14
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-02-26 16:20:06 -0300

    feat: add multiplicar method in calculadora class

--- src/main/java/br/com/zup/Calculadora.java
+++ src/main/java/br/com/zup/Calculadora.java
@@ -12,4 +12,8 @@ public class Calculadora {
     public Double dividir(Double n1, Double n2) {
         return n1 / n2;
     }
+
+    public Double multiplicar(Double n1, Double n2) {
+        return n1 * n2;
+    }
 }

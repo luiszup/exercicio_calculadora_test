6ea74436fda74aef4adad7ef260a3aaadd9e04f1
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-02-26 16:22:42 -0300

    refactor: add calculadora constructor in main class

--- src/main/java/br/com/zup/Main.java
+++ src/main/java/br/com/zup/Main.java
@@ -6,6 +6,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        Calculadora calculadora = new Calculadora();
+
         System.out.println("Digite o primeiro número: ");
         double numero1 = input.nextInt();
         System.out.println("Digite o segundo número: ");
@@ -14,15 +16,15 @@ public class Main {
         char operador = input.next().charAt(0);
 
         if (operador == '+') {
-            System.out.println(numero1 + " + " + numero2 + " = " + (numero1 + numero2));
+            System.out.println(numero1 + " + " + numero2 + " = " + (calculadora.somar(numero1, numero2)));
         } else if (operador == '-') {
-            System.out.println(numero1 + " - " + numero2 + " = " + (numero1 - numero2));
+            System.out.println(numero1 + " - " + numero2 + " = " + (calculadora.subtrair(numero1, numero2)));
         } else if (operador == '/') {
-            System.out.println(numero1 + " / " + numero2 + " = " + (numero1 / numero2));
+            System.out.println(numero1 + " / " + numero2 + " = " + (calculadora.dividir(numero1, numero2)));
         } else if (operador == '*') {
-            System.out.println(numero1 + " * " + numero2 + " = " + (numero1 * numero2));
+            System.out.println(numero1 + " * " + numero2 + " = " + (calculadora.multiplicar(numero1, numero2)));
         } else if (operador == '%') {
-            System.out.println(numero1 + " % " + numero2 + " = " + (numero1 % numero2));
+            System.out.println(numero1 + " % " + numero2 + " = " + (calculadora.modulo(numero1, numero2)));
         } else {
             System.out.println("Operador inválido! Escreva uma das opções apresentadas.");
         }
